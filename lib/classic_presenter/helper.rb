a2d47cd990208e02b004ca0c988054ca04a9dfe9
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-06-20 22:01:34 -0300

    using default params

--- lib/classic_presenter/helper.rb
+++ lib/classic_presenter/helper.rb
@@ -2,7 +2,7 @@ module ClassicPresenter
   module Helper
     extend self
 
-    def present(presenter_class, model, objects)
+    def present(presenter_class, model = nil, objects = {})
       presenter = presenter_class.new(model, self, objects)
       yield presenter if block_given?
       presenter
